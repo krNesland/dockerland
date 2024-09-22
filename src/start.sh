03b939175b4047546aa56a1705aacd8f918a209d
Kristoffer Nesland <kristoffer@solutionseeker.no>
2024-09-22 13:59:28 +0200

    Making it ready for fastapi

---
+++ src/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ls
+eval "$(conda shell.bash hook)"
+conda activate dockerland
+conda env list
+conda list
+python -c "print('hello world')"
+fastapi dev src/fast_demo.py --host 0.0.0.0 --port 8000  # Running the app and making it available on the network
