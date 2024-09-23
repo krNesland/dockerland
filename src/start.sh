4de329c715ad52e9ca028ef72077d240b79a9652
Kristoffer Nesland <kristoffer@solutionseeker.no>
2024-09-23 08:47:56 +0200

    Add dev container config

--- src/start.sh
+++ src/start.sh
@@ -5,4 +5,5 @@ conda activate dockerland
 conda env list
 conda list
 python -c "print('hello world')"
+python "src/main.py"
 fastapi dev src/fast_demo.py --host 0.0.0.0 --port 8000  # Running the app and making it available on the network
